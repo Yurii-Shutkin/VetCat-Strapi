f994f87595bfbc94f18ba8d48bee4def09719999
Yurii-Shutkin <yurashutkinn@gmail.com>
2026-02-14 00:20:15 +0200

    init strapi

---
+++ src/api/services-card/controllers/services-card.ts
@@ -0,0 +1,7 @@
+/**
+ * services-card controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::services-card.services-card');
---
+++ src/api/services-card/routes/services-card.ts
@@ -0,0 +1,7 @@
+/**
+ * services-card router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::services-card.services-card');
